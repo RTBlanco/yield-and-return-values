203fa7bdb0373b4c0ee6860a37386c8e69eb0aee
RTBlanco <ronnytoribio1@hotmail.com>
2020-10-04 03:25:33 -0400

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,11 +2,32 @@ require 'pry'
 
 def hello(array)
   i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+def hello2(array)
+  i = 0 
   while i < array.length
     yield(array[i])
     i += 1
   end
-end
+end 
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+# new_lsit = hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+# puts new_lsit.inspect
+
+test_list = ["Tim", "Tom", "Jim"]
+
+another_list = hello2(test_list) {|n| "Hey #{n}"}
+
+compare_list = hello(test_list) {|n| "Hey #{n}"}
+
+puts another_list
+
+# puts compare_list 
